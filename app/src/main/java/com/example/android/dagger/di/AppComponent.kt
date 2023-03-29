a4894b33e53a6e6cd49ae88b5f4f2cb652c6c0df
sotfheif <vovmart98@gmail.com>
2023-03-30 01:35:32 +0400

    finished step 11

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -3,18 +3,21 @@ package com.example.android.dagger.di
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [StorageModule::class])
+@Component(modules = [StorageModule::class, AppSubcomponents::class])
 interface AppComponent{
      @Component.Factory
      interface Factory {
           fun create(@BindsInstance context: Context): AppComponent
      }
-     fun inject(activity: RegistrationActivity)
+     fun registrationComponent(): RegistrationComponent.Factory
      fun inject(activity: MainActivity)
 
 }
--- app/src/main/java/com/example/android/dagger/registration/RegistrationActivity.kt
+++ app/src/main/java/com/example/android/dagger/registration/RegistrationActivity.kt
@@ -27,13 +27,17 @@ import com.example.android.dagger.registration.termsandconditions.TermsAndCondit
 import javax.inject.Inject
 
 class RegistrationActivity : AppCompatActivity() {
+    lateinit var registrationComponent: RegistrationComponent
 
     // @Inject annotated fields will be provided by Dagger
     @Inject lateinit var registrationViewModel: RegistrationViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Ask Dagger to inject our dependencies
-        (application as MyApplication).appComponent.inject(this)
+        //(application as MyApplication).appComponent.inject(this)
+        registrationComponent = (application as MyApplication).appComponent.registrationComponent().create()
+        registrationComponent.inject(this)
+
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
