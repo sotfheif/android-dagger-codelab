a533494194bc32896cebf1a1a1e18ff4907d992f
sotfheif <vovmart98@gmail.com>
2023-03-30 13:17:46 +0400

    first steps to refactor SettingsActivity to use Dagger

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -7,6 +7,7 @@ import com.example.android.dagger.registration.RegistrationActivity
 import com.example.android.dagger.registration.RegistrationComponent
 import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
 import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
+import com.example.android.dagger.settings.SettingsActivity
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -21,5 +22,6 @@ interface AppComponent{
      fun registrationComponent(): RegistrationComponent.Factory
      fun loginComponent(): LoginComponent.Factory
      fun inject(activity: MainActivity)
+     fun inject(activity: SettingsActivity)
 
 }
