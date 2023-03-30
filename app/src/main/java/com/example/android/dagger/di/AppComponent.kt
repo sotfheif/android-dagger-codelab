faa56c854dccb35c86c468351928f1f9df040501
sotfheif <vovmart98@gmail.com>
2023-03-30 13:12:26 +0400

    probably correctly finished step 12

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.example.android.dagger.di
 
 import android.content.Context
+import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
 import com.example.android.dagger.registration.RegistrationComponent
@@ -18,6 +19,7 @@ interface AppComponent{
           fun create(@BindsInstance context: Context): AppComponent
      }
      fun registrationComponent(): RegistrationComponent.Factory
+     fun loginComponent(): LoginComponent.Factory
      fun inject(activity: MainActivity)
 
 }
